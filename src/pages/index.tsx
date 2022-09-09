20996c35c9e64ae5ed4eb655231167c7d4ef34f5
DeyvisonTav <deyvisontav@gmail.com>
2022-09-09 16:30:50 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import Layout from '../components/template/Layout'
+export default function Home() {
+  return (
+    <Layout titulo="Página Inicial" subtitulo="Estamos construindo um template Admin!">
+      <h3 className='pr-10 pt-10' >Gráfico!</h3>
+    </Layout>
+  )
+}
---
+++ src/pages/notificacoes.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/template/Layout'
+
+export default function Notificacoes() {
+  return (
+    <Layout titulo="Notificações" 
+      subtitulo="Aqui você irá gerenciar as suas notificações!">
+        <h1 className='pr-10 pt-10'>Suas notificações em criptomoedas</h1>
+    </Layout>
+  )
+}
